a4e82c9823528a0d38de1c0d061f891d529134a7
jesse <jufc.29@gmail.com>
2025-05-11 15:33:10 -0600

    url modificacion

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,7 @@
 import psycopg2
 
-URL="postgresql://jesse:NuIyzDWW4PVYHCF7HVRjPrKdMdzGbdi0@dpg-d0d4vq15pdvs73f7o7ag-a/pokemones_nqom"
+URL="postgresql://jesse:NuIyzDWW4PVYHCF7HVRjPrKdMdzGbdi0@dpg-d0d4vq15pdvs73f7o7ag-a.oregon-postgres.render.com/pokemones_nqom"
+
 
 conn = psycopg2.connect(URL)
 cursor=conn.cursor()
@@ -9,11 +10,12 @@ cursor.execute("""DROP TABLE kanto;""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS kanto (
                id SERIAL PRIMARY KEY,
-               numero NUMERIC,
-               nombre VARCHAR(50), 
+               numero NUMERIC UNIQUE,
+               nombre VARCHAR(50) UNIQUE, 
                tipo1 VARCHAR (50),
                tipo2 VARCHAR (50))"""
                )
+
 
 conn.commit()
 cursor.close()
